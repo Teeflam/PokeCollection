dea0cf6a4ec267ec4502fe555d253a9e3d5d8bcf
Lucas <lucas.briere@efrei.net>
2021-03-28 16:39:19 +0200

    Feat: interaction with API

--- PokeCollection/src/app/description/description.component.ts
+++ PokeCollection/src/app/description/description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-description',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DescriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getPokemon()
+    .subscribe((response: any) => {
+      console.log(response);
+    });
   }
 
 }
