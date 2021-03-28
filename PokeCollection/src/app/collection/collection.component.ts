29aa40f4d846a42a7fc376e8c47fdee4abd2cf97
Lucas <lucas.briere@efrei.net>
2021-03-28 19:11:45 +0200

    Feat: retreive collection in API

--- PokeCollection/src/app/collection/collection.component.ts
+++ PokeCollection/src/app/collection/collection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-collection',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collection.component.css']
 })
 export class CollectionComponent implements OnInit {
+  pokemons: any[] = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getPokemon()
+    .subscribe((response: any) => {
+      response.results.forEach ((result: { name: string; }) => {
+        this.dataService.getMoreData(result.name)
+        .subscribe((uniqResponse: any) => {
+          this.pokemons.push(uniqResponse);
+        });
+      });
+    });
+    console.log(this.pokemons);
   }
-
 }
