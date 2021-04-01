9b0d295262b9221837f0500530e1a75f77a5266a
giornotheme <vtang@outlook.fr>
2021-04-01 17:25:05 +0200

    home page commit

--- PokeCollection/src/app/home/home.component.ts
+++ PokeCollection/src/app/home/home.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent{
+  @Input() pokemonList = [] as string[];
+
+  pokeGen: any[] = [];
+
+  condInd: number = 0;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.doSomething(changes['pokemonList'].currentValue.length);
+  }
+  ngOnInit(): void {
+    this.pokeGen = this.dataService.sortPokemonByGene(this.pokemonList);
+  }
+
+  tabChange(index: number) {
+    this.condInd = index;
+  }
+  doSomething(length: number) {
+    this.pokeGen = this.dataService.sortPokemonByGene(this.pokemonList);
+  }
+}
