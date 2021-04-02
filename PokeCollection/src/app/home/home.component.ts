30a45cdbb7c363ab2c5a81555dd9a07d5c5a864a
Lucas <lucas.briere@efrei.net>
2021-04-02 22:48:13 +0200

    enable button if user is connected

--- PokeCollection/src/app/home/home.component.ts
+++ PokeCollection/src/app/home/home.component.ts
@@ -1,27 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import Poke from '../models/Poke';
+import { AuthService } from '../services/Auth/auth.service';
 import { DataService } from '../services/Data/data.service';
-
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css'],
 })
-
-export class HomeComponent{
-	
+export class HomeComponent implements OnInit, OnDestroy {
 	pokemonList: Poke[] = [];
+	userID: string | undefined;
+	userIDSubscription: Subscription;
 
-	constructor(private dataService: DataService) {}
+	constructor(
+		private dataService: DataService,
+		private authService: AuthService
+	) {
+		this.userID = this.authService.currentUserId;
+		this.userIDSubscription = this.authService.userIDObservable.subscribe(
+			(userID) => {
+				this.userID = userID;
+			}
+		);
+	}
 
 	ngOnInit(): void {
 		for (let i = 1; i < 6; i++) {
 			this.dataService
-			.getMoreData(i.toString())
-			.then((uniqResponse: Poke) => {
-				this.pokemonList.push(uniqResponse);
-			});
+				.getMoreData(i.toString())
+				.then((uniqResponse: Poke) => {
+					this.pokemonList.push(uniqResponse);
+				});
 		}
+	}
+
+	ngOnDestroy(): void {
+		this.userIDSubscription.unsubscribe();
 	}
 }
