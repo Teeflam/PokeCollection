803a5f302c29fce7c9ac7a056333fc4153830048
James <james.jin@efrei.net>
2021-04-05 18:23:17 +0200

    feat: add comments and fix routerLink in Home page

--- PokeCollection/src/app/navigation/navigation.component.ts
+++ PokeCollection/src/app/navigation/navigation.component.ts
@@ -9,22 +9,24 @@ import { AuthService } from '../services/Auth/auth.service';
 	styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnDestroy {
+	//variable to use for connection
 	userID: string | undefined;
 	userIDSubscription: Subscription;
 
 	constructor(private router: Router, private authService: AuthService) {
 		this.userID = this.authService.currentUserId;
+		//subscribe to the Observable of userID 
 		this.userIDSubscription = this.authService.userIDObservable.subscribe(
 			(userID) => {
 				this.userID = userID;
 			}
 		);
 	}
-
+  //unsubscribe to userID Observable, to avoid overflow
 	ngOnDestroy() {
 		this.userIDSubscription.unsubscribe();
 	}
-
+	//logout 
 	async logout() {
 		await this.authService.signOut();
 		await this.router.navigateByUrl('/');
